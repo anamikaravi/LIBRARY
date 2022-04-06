1881b0440ff0b1e1b8376df9b9c167f20915eb75
anamikaravi <72075104+anamikaravi@users.noreply.github.com>
2022-04-06 15:21:57 +0530

    Add files via upload

---
+++ library/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.contrib.auth.models import User
+from . import models
+
+class ContactusForm(forms.Form):
+    Name = forms.CharField(max_length=30)
+    Email = forms.EmailField()
+    Message = forms.CharField(max_length=500,widget=forms.Textarea(attrs={'rows': 3, 'cols': 30}))
+class AdminSigupForm(forms.ModelForm):
+    class Meta:
+        model=User
+        fields=['first_name','last_name','username','password']
+class BookForm(forms.ModelForm):
+    class Meta:
+        model=models.Book
+        fields=['name','isbn','id','author','category']
